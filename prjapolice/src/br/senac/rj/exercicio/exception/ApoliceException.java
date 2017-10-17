0e5be26ac43866d8ab7a56f9331ed60db42321c9
Marcos S. Ferreira <msferreira.prof@gmail.com>
2017-10-17 16:17:48 -0200

    Criar Exception propria

---
+++ prjapolice/src/br/senac/rj/exercicio/exception/ApoliceException.java
@@ -0,0 +1,29 @@
+package br.senac.rj.exercicio.exception;
+
+public class ApoliceException extends Exception {
+
+	public ApoliceException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApoliceException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApoliceException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApoliceException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApoliceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
